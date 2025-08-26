7ccf13fe138c7714a84b55abe18e960038f5e938
Elizabeth Pretto-Sotelo ⊹.✮₊⋆ <elizabethprettosotelo@gmail.com>
2025-08-26 12:23:24 -0400

    fixed type problem & spacing issues

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
 	toggleBtn.addEventListener('click', function() {
 		document.body.classList.toggle('dark-mode');
 	});
+
+	// Setup GIF hover effects for experience cards
+	const experienceImages = document.querySelectorAll('.experience-img[data-gif]');
+	experienceImages.forEach(img => {
+		const gifUrl = img.getAttribute('data-gif');
+		img.style.setProperty('--gif-url', `url('${gifUrl}')`);
+	});
 });
